04f4ded06f96212e57d72fbea6ad3bf2330f5f7e
matousekjakub <matousekjakub@protonmail.com>
2022-11-08 18:05:28 +0100

    added new data

--- src/data.js
+++ src/data.js
@@ -4,4 +4,5 @@ export const PRODUCTS = [
     {id:2, category: "Pračky", price: 11005, stocked: true, name: "Pracka FG Turboboost 5000", img: "pracka_fg_ultra_bezskvrny_5000.png"},
     {id:3, category: "Sluchátka", price: 8599, stocked: true, name: "Sluchátka TUC bassaddict V2.0", img: "sluchatka_1.png"},
     {id:4, category: "Sluchátka", price: 5990, stocked: false, name: "Sluchátka MRKEW vzduchpod 2020", img: "sluchatka_mrkev_vzduchpod.png"},
+    {id:5, category: "Sluchátka", price: 10499, stocked: true, name: "Sluchátka Hearloss 300W", img: "sluchatka_2.png"}
 ]  
